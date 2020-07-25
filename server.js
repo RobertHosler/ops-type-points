855a537264d6e8c7cd1d37b1540ce13ee52d84cb
Robert Hosler <rph731@gmail.com>
2020-07-25 03:06:38 -0400

    Server js moved to root

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/ops-type-points'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/ops-type-points/'
+}
+);
 });
 
 // Start the app by listening on the default Heroku port
