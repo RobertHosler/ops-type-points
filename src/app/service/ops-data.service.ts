2b05508fd89c1944c70a880b31201114db0381a5
Robert Hosler <rph731@gmail.com>
2021-09-09 14:21:46 -0400

    GetList refactor and GetType

--- src/app/service/ops-data.service.ts
+++ src/app/service/ops-data.service.ts
@@ -3,93 +3,96 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpsDataService {
-
-
   baseUrl = 'https://ops-data-service.herokuapp.com';
-  // baseUrl = 'http://localhost:8080';
   greetingPath = '/greeting';
   airtablePath = '/airtable';
   recordsPath = '/opsRecords';
   coinsPath = '/tenCoins';
   namePath = '/name';
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
-  getGreeting():Observable<Greeting> {
-    return this.httpClient.get<Greeting>(this.baseUrl + this.greetingPath,
-      { 'headers': new HttpHeaders(
-          {
-            'Content-Type':'application/json; charset=utf-8',
-          }
-        )
-      } );
+  getGreeting(): Observable<Greeting> {
+    return this.httpClient.get<Greeting>(this.baseUrl + this.greetingPath, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=utf-8',
+      }),
+    });
   }
 
   getAirtable() {
-    return this.httpClient.get(this.baseUrl + this.airtablePath,
-      { 'headers': new HttpHeaders(
-          {
-            'Content-Type':'application/json; charset=utf-8',
-          }
-        )
-      } );
+    return this.httpClient.get(this.baseUrl + this.airtablePath, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=utf-8',
+      }),
+    });
   }
 
-  getName(maxRecords: number, name: string):Observable<TypeRoot> {
-    return this.httpClient.get<TypeRoot>(this.baseUrl + this.namePath,
-      { 'headers': new HttpHeaders(
-          {
-            'Content-Type':'application/json; charset=utf-8',
-          }
-        ),
-        'params': {
-          'name': name,
-          'maxRecords': maxRecords
-        }
-      });
+  getName(maxRecords: number, name: string): Observable<TypeRoot> {
+    return this.getList(this.namePath, {
+      maxRecords: maxRecords,
+      name: name,
+    });
   }
 
-  getCoins(maxRecords: number, hn1: string, ohn: string, dhn: string,
-      ol: string, dl: string, ia: string, ea: string, dom: string, smod: string, demod):Observable<TypeRoot> {
-    return this.httpClient.get<TypeRoot>(this.baseUrl + this.coinsPath,
-      { 'headers': new HttpHeaders(
-          {
-            'Content-Type':'application/json; charset=utf-8',
-          }
-        ),
-        'params': {
-          'hn1': hn1,
-          'ohn': ohn,
-          'dhn': dhn,
-          'ol': ol,
-          'dl': dl,
-          'ia': ia,
-          'ea': ea,
-          'dom': dom,
-          'smod': smod,
-          'demod': demod,
-          'maxRecords': maxRecords
-        }
-      });
+  getType(maxRecords: number, type: string): Observable<TypeRoot> {
+    return this.getList('/list', {
+      maxRecords: maxRecords,
+      type: type,
+    });
   }
 
-  getRecords(s1: string, s2: string, animalStack: string):Observable<TypeRoot> {
-    return this.httpClient.get<TypeRoot>(this.baseUrl + this.recordsPath,
-      {
-        'headers': new HttpHeaders({
-            'Content-Type':'application/json; charset=utf-8',
-        }),
-        'params': {
-          'maxRecords': '100',
-          's1': s1,
-          's2': s2,
-          'as': animalStack
-        }
-      });
+  private getList(path: string, params): Observable<TypeRoot> {
+    return this.httpClient.get<TypeRoot>(this.baseUrl + this.namePath, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=utf-8',
+      }),
+      params: params,
+    });
+  }
+
+  getCoins(
+    maxRecords: number,
+    hn1: string,
+    ohn: string,
+    dhn: string,
+    ol: string,
+    dl: string,
+    ia: string,
+    ea: string,
+    dom: string,
+    smod: string,
+    demod: string
+  ): Observable<TypeRoot> {
+    return this.getList(this.coinsPath, {
+      hn1: hn1,
+      ohn: ohn,
+      dhn: dhn,
+      ol: ol,
+      dl: dl,
+      ia: ia,
+      ea: ea,
+      dom: dom,
+      smod: smod,
+      demod: demod,
+      maxRecords: maxRecords
+    });
+  }
+
+  getRecords(
+    s1: string,
+    s2: string,
+    animalStack: string
+  ): Observable<TypeRoot> {
+    return this.getList(this.recordsPath, {
+      maxRecords: '100',
+      s1: s1,
+      s2: s2,
+      as: animalStack
+    });
   }
 }
 
@@ -99,7 +102,7 @@ export class Greeting {
 }
 
 export class TypeRoot {
-  records:TypeRecord[];
+  records: TypeRecord[];
 }
 export class TypeRecord {
   id: string;
