cc5886be02e1f25f558ad7afef934ec1014c5676
Robert Hosler <rph731@gmail.com>
2021-10-15 00:12:55 -0400

    Community Member Inclusions

--- server/ops-typed-persons.js
+++ server/ops-typed-persons.js
@@ -1,38 +1,53 @@
 /*jshint esversion: 6 */
 
-const opsKey = process.env.OP_DATABASE_KEY || require('./local-api').key;
-const TERMS_HOST = 'https://api.airtable.com/v0/appudq0aG1uwqIFX5/';
+const opsKey = process.env.OP_DATABASE_KEY || require("./local-api").key;
+const TERMS_HOST = "https://api.airtable.com/v0/appudq0aG1uwqIFX5/";
 const MAX_RECORD = 10000;
 
-const listUrl = new URL(TERMS_HOST + 'Officially Typed People');
-listUrl.searchParams.append('api_key', opsKey);
-listUrl.searchParams.append('maxRecords', MAX_RECORD);
-listUrl.searchParams.append('view', 'Gallery by MBTI Type');
-listUrl.searchParams.append('fields', 'Name');
-listUrl.searchParams.append('fields', 'Type');
-listUrl.searchParams.append('fields', 'Tags');
-listUrl.searchParams.append('fields', 'Picture');
+const listUrl = new URL(TERMS_HOST + "Officially Typed People");
+listUrl.searchParams.append("api_key", opsKey);
+listUrl.searchParams.append("maxRecords", MAX_RECORD);
+listUrl.searchParams.append("view", "Gallery by MBTI Type");
+listUrl.searchParams.append("fields", "Name");
+listUrl.searchParams.append("fields", "Type");
+listUrl.searchParams.append("fields", "Tags");
+listUrl.searchParams.append("fields", "Picture");
 //Coins
-listUrl.searchParams.append('fields', 'Sensory Sexual');
-listUrl.searchParams.append('fields', 'De Sexual');
-listUrl.searchParams.append('fields', 'Savior Observer');
-listUrl.searchParams.append('fields', 'Savior Decider');
-listUrl.searchParams.append('fields', 'Single Observer vs Decider');
-listUrl.searchParams.append('fields', 'Decider Human Need');
-listUrl.searchParams.append('fields', 'Observer Human Need');
-listUrl.searchParams.append('fields', 'Energy vs Info Dom');
-listUrl.searchParams.append('fields', 'Blast vs Consume');
-listUrl.searchParams.append('fields', 'Play vs Sleep');
-listUrl.searchParams.append('fields', 'Biological Sex');
-listUrl.searchParams.append('fields', 'Transgender');
+listUrl.searchParams.append("fields", "Sensory Sexual");
+listUrl.searchParams.append("fields", "De Sexual");
+listUrl.searchParams.append("fields", "Savior Observer");
+listUrl.searchParams.append("fields", "Savior Decider");
+listUrl.searchParams.append("fields", "Single Observer vs Decider");
+listUrl.searchParams.append("fields", "Decider Human Need");
+listUrl.searchParams.append("fields", "Observer Human Need");
+listUrl.searchParams.append("fields", "Energy vs Info Dom");
+listUrl.searchParams.append("fields", "Blast vs Consume");
+listUrl.searchParams.append("fields", "Play vs Sleep");
+listUrl.searchParams.append("fields", "Biological Sex");
+listUrl.searchParams.append("fields", "Transgender");
 
 const urlMap = new Map();
-urlMap.set('list', listUrl);
+urlMap.set("list", listUrl);
 
 // Exclude select names
-const exclusionsList = ['Jesus'];
+const exclusionsList = [
+  "Jesus",
+  "Sarah",
+  // --Christopher Columbus
+  "Batman",
+  "The Joker",
+  "Superman",
+  "Gargamel (Smurfs)",
+];
 // Include select community members
-const inclusionsList = ['Roqb Hosler'];
+const inclusionsList = [
+  "Roqb Hosler",
+  "Evelien S. Vandenberghe",
+  "Laura Miller",
+  "Jana Leigh",
+  "Sam Kelley",
+  "Aiste Jonusaite",
+];
 
 exports.urlMap = urlMap;
 exports.exclusions = exclusionsList;
