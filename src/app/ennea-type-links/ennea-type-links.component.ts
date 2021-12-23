304db5c94553bc41138d9c66dd25f857dd58c311
Robert Hosler <rph731@gmail.com>
2021-12-23 13:40:44 +0200

    Notes and Generic Links

--- src/app/ennea-type-links/ennea-type-links.component.ts
+++ src/app/ennea-type-links/ennea-type-links.component.ts
@@ -44,17 +44,38 @@ export class EnneaTypeLinksComponent implements OnInit {
           }
         }
       });
-      let link = new Link();
-      link.text = "Join DAA";
-      link.href = 'https://www.enneagrammer.com/members-home';
-      this.daaLinks.push(link);
-      let publicDaa = new Link();
-      publicDaa.text = "Public DAA Content";
-      publicDaa.href = 'https://vimeo.com/user146722472?embedded=true&source=owner_name&owner=146722472';
-      this.daaLinks.push(publicDaa);
+    }
+    let link = new Link();
+    link.text = "Join DAA";
+    link.href = 'https://www.enneagrammer.com/members-home';
+    this.daaLinks.push(link);
+    let publicDaa = new Link();
+    publicDaa.text = "Public DAA Content";
+    publicDaa.href = 'https://vimeo.com/user146722472?embedded=true&source=owner_name&owner=146722472';
+    this.daaLinks.push(publicDaa);
+    let bhe = new Link();
+    bhe.text = "Essential BHE Playlist";
+    bhe.href = 'https://www.youtube.com/playlist?list=PLWefbotJmUuyUqeXZYR8D-7XA2NXvbv-V';
+    this.daaLinks.push(bhe);
+    if (!this.typedPerson.tags.includes('Community Member')) {
+      let searchLink = new Link();
+      searchLink.text = 'Google Search';
+      searchLink.href =
+        'https://www.google.com/search?q=' + this.typedPerson.name;
+      this.daaLinks.push(searchLink);
+      let imageLink = new Link();
+      imageLink.text = 'Image Search';
+      imageLink.href =
+        'https://www.google.com/search?tbm=isch&q=' + this.typedPerson.name;
+      this.daaLinks.push(imageLink);
+      let interviewLink = new Link();
+      interviewLink.text = 'Interview Search';
+      interviewLink.href =
+        'https://www.youtube.com/results?search_query=interview ' +
+        this.typedPerson.name;
+      this.daaLinks.push(interviewLink);
     }
   }
-
 
   private validYoutubeLink(href: string) {
     return (
