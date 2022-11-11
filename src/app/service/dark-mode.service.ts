7286a1f45a5b004f1205c5c5e2da0d1ad49cb83b
Robert Hosler <rph731@gmail.com>
2022-11-10 22:49:11 -0500

    dark mode at night

--- src/app/service/dark-mode.service.ts
+++ src/app/service/dark-mode.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { Injectable, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,11 @@ export class DarkModeService {
 
   constructor(rendererFactory: RendererFactory2) {
     this.renderer = rendererFactory.createRenderer(null, null);
-}
+    const h = new Date().getHours();
+    if (h < 7 || h > 17) {
+      this.toggleDarkMode();
+    }
+  }
 
   toggleDarkMode() {
     if (this.darkMode) {
