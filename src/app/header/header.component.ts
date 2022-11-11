34c0876d76588b5c78bb030937ea58259e3f58b4
Robert Hosler <rph731@gmail.com>
2022-11-11 11:48:03 -0500

    Navbar updates

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -94,6 +94,59 @@ export class HeaderComponent implements OnInit {
     ),
   ];
   
+  externalLinks: HomeLink[] = [
+    new HomeLink(
+      'Directory of OPS Links',
+      'https://docs.google.com/document/d/1h5MkfI1KpnVZl2AWaUnm45Nkt6A12F7Bgi3CFZkXinM/edit#',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'OPT Toy',
+      'https://opt-toy.vercel.app/',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'OPS Database',
+      'http://db.subjectivepersonality.com/',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'Class Notes',
+      'https://docs.google.com/document/d/1aLrwLdhvOGIF-fdouUxAxJ_5tlt6mgu_NBnXaN4N_JU/edit?usp=sharing',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'Class Archives',
+      'https://docs.google.com/document/d/1lNO4NjYwvPQYSBhHZNzTiFTTVfhitOWlFsdvr-WTX9g/edit?usp=sharing',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'Type-A-Tool',
+      'https://docs.google.com/document/d/1CcoxI6aknkRsc5-LCb41Ire-jGNCsqIKA76oeY4cxsQ/edit#',
+      '',
+      false,
+      ''
+    ),
+    new HomeLink(
+      'Elimination Tool',
+      'https://www.objectivepersonalitysystem.com/elimination-tool',
+      '',
+      false,
+      ''
+    ),
+    
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
