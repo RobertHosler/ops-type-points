7bb33f562732dd323eaa99193ad2f3f907887053
Robert Hosler <rph731@gmail.com>
2023-04-30 22:14:30 -0400

    Angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -90,9 +90,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      relativeLinkResolution: 'legacy',
-      scrollPositionRestoration: 'enabled',
-    }),
+    scrollPositionRestoration: 'enabled'
+}),
   ],
   exports: [RouterModule],
 })
