3336ad59ffe669c27e70cd66db7bd474208bb67a
RobertHosler <rph731@gmail.com>
2024-03-25 21:01:19 -0400

    Embed AP Youtube Video

--- src/app/ap-links/ap-links.component.ts
+++ src/app/ap-links/ap-links.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { apModel } from '../model/ap-model';
 import { TypedPerson } from '../service/ops-data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-ap-links',
@@ -14,6 +15,7 @@ export class ApLinksComponent {
 
   apModel = apModel;
   aspects : string[];
+  sanitizedEmbed: SafeResourceUrl;
   
   apType : {
     name: string;
@@ -21,10 +23,39 @@ export class ApLinksComponent {
     descriptionLink: string;
     sextaLink: string;
   };
+
+  constructor(private _sanitizer: DomSanitizer) {}
   
   ngOnInit(): void {
     this.apType = apModel.apTypeMap.get(this.typedPerson.apType);
     this.aspects = this.apType.name.split('');
+
+    if (this.typedPerson.apLink) {
+      this.sanitizedEmbed = this.sanitize(this.typedPerson.apLink);
+    }
+
+  }
+
+  sanitize(url: string) {
+    let youtubeIndex = url.indexOf('youtube.com/');
+    let embedUrl = null;
+    if (youtubeIndex > 0) {
+      embedUrl = url.replace('watch?v=', 'embed/');
+    } else {
+      youtubeIndex = url.indexOf('youtu.be/');
+      if (youtubeIndex > 0) {
+        embedUrl = url.replace('youtu.be/', 'youtube.com/embed/');
+      }
+    }
+    if (embedUrl) {
+      let ampIndex = embedUrl.indexOf('&');
+      if (ampIndex > 0) {
+        embedUrl = embedUrl.substring(0, ampIndex);
+      }
+      return this._sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+    } else {
+      return null;
+    }
   }
 
 }
